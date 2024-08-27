5a3c1e3339851a4964cf622b538146220e1ca574
Lucas Fellini <152074771+LucasFellini@users.noreply.github.com>
2024-08-27 16:25:10 -0300

    Add files via upload
    // 3) Verifique se a variável x é igual a 3.

---
+++ EXC03L2.c
@@ -0,0 +1,17 @@
+// 3) Verifique se a variável x é igual a 3.
+
+
+#include <stdio.h> 
+
+int main (){
+
+    int x;
+
+    printf("informe o valor de x: ");
+    scanf("%d", &x);
+
+    if (x == 3)
+    printf("X é igual a 3");
+    else
+    printf("X não é igual a 3");
+}
