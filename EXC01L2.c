2246a43e331079555688cd68667fab55d780884a
Lucas Fellini <152074771+LucasFellini@users.noreply.github.com>
2024-08-27 16:24:20 -0300

    Add files via upload
    //1) Faça um programa que receba um número e verifique se é par ou ímpar

---
+++ EXC01L2.c
@@ -0,0 +1,18 @@
+//1) Faça um programa que receba um número e
+//verifique se é par ou ímpar
+
+#include <stdio.h> 
+
+int main () {
+
+    int  x ;
+
+        printf("informe um número:");
+        scanf("%d", &x);
+
+        if (x % 2 == 0);
+        printf("é par");
+        else
+        printf("é impar");
+
+}
